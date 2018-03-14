7650f4c5c9dbfd091d11e6b80594d372a32df259
binwangn <binwangn@isoftstone.com>
2018-03-14 11:56:38 +0800

    Upload String usage code

---
+++ 2/2-string/str.java
@@ -0,0 +1,12 @@
+/*
+ * String usage
+ */
+class Str{
+	String m_strBscShp = "\u2588";
+
+	void prtFShp(){
+		System.out.println((char)254 + (char)254);
+		System.out.println(m_strBscShp);
+//		System.out.println(cBasicShap);
+	}
+}
---
+++ 2/2-string/strDemo.java
@@ -0,0 +1,10 @@
+/*
+ * String demo
+ */
+class StrDemo{
+	public static void main(String[] args){
+		Str oStr = new Str();
+
+		oStr.prtFShp();
+	}
+}
