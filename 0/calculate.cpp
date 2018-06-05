eec3ccb7c8d0d5c786b1957efa5042f85e630824
Kingmier centos <kingmier@hotmail.com>
2018-06-05 12:09:04 +0800

    Archieve method of output-bin

--- 0/calculate.cpp
+++ 0/calculate.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<string>
 #include<iomanip>
-//#include<conio.h>
 #include<cmath>
 #include<stdio.h>
 
@@ -14,9 +13,37 @@ Scale::Scale()
 }
 void Scale::output_bin(int n)
 {
-	char s[60];
+	char s[60] = {0};
 	//itoa(n, s, 2);
-	sprintf(s, "%d", n);
+	int nTmp = n;
+	int iCnt = 0;
+	while (1)
+	{
+		if (0 == nTmp)
+		{
+			break;
+		}
+
+		int iMode = nTmp % 2;
+		if (iCnt < 60 - 1)
+		{
+			int iCntTmp = iCnt;
+			do
+			{
+				s[iCntTmp + 1] = s[iCntTmp];
+				iCntTmp--;
+			}while(iCntTmp >= 0);
+			s[0] = iMode + '0';
+			iCnt++;
+		}
+		else
+		{
+			break;
+		}
+
+		nTmp = nTmp / 2;
+	}
+	
 	cout << n << "--" << s << endl;
 }
 void Scale::output_hex(int n)
